4f3c524cdd17d3ecc0415729764d6659f9150d96
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2026-05-21 23:42:36 +0100

    Create Low Pop.js

---
+++ App Sounds/Low Pop.js
@@ -0,0 +1,23 @@
+function playPop(ctx, volume = 0.7) {
+  const osc = ctx.createOscillator();
+  const gain = ctx.createGain();
+  osc.connect(gain);
+  gain.connect(ctx.destination);
+
+  osc.type = 'sine';
+  osc.frequency.setValueAtTime(300, ctx.currentTime);
+  osc.frequency.exponentialRampToValueAtTime(
+    80, ctx.currentTime + 0.08
+  );
+
+  gain.gain.setValueAtTime(0.0001, ctx.currentTime);
+  gain.gain.linearRampToValueAtTime(
+    volume, ctx.currentTime + 0.005
+  );
+  gain.gain.exponentialRampToValueAtTime(
+    0.0001, ctx.currentTime + 0.08
+  );
+
+  osc.start(ctx.currentTime);
+  osc.stop(ctx.currentTime + 0.08);
+}
