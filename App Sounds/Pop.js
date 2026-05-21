e60e9756b7ce8a3b548038a43c6421ba4c0df67c
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2026-05-21 23:41:23 +0100

    Create Pop.js

---
+++ App Sounds/Pop.js
@@ -0,0 +1,20 @@
+function playTick(ctx, volume = 0.6) {
+  const osc = ctx.createOscillator();
+  const gain = ctx.createGain();
+  osc.connect(gain);
+  gain.connect(ctx.destination);
+
+  osc.type = 'sine';
+  osc.frequency.setValueAtTime(1200, ctx.currentTime);
+  osc.frequency.exponentialRampToValueAtTime(
+    800, ctx.currentTime + 0.04
+  );
+
+  gain.gain.setValueAtTime(volume, ctx.currentTime);
+  gain.gain.exponentialRampToValueAtTime(
+    0.0001, ctx.currentTime + 0.04
+  );
+
+  osc.start(ctx.currentTime);
+  osc.stop(ctx.currentTime + 0.04);
+}
