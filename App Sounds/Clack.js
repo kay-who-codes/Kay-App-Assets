9be243eafcda06da77e76319ea9ff3cc59d1e4ef
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2026-05-21 23:42:58 +0100

    Add playClack function for sound playback

---
+++ App Sounds/Clack.js
@@ -0,0 +1,25 @@
+function playClack(ctx, volume = 1.2) {
+  const bufSize = Math.floor(ctx.sampleRate * 0.015);
+  const buf = ctx.createBuffer(1, bufSize, ctx.sampleRate);
+  const data = buf.getChannelData(0);
+
+  for (let i = 0; i < bufSize; i++) {
+    data[i] = (Math.random() * 2 - 1) * (1 - i / bufSize);
+  }
+
+  const src = ctx.createBufferSource();
+  const gain = ctx.createGain();
+  const filter = ctx.createBiquadFilter();
+
+  filter.type = 'bandpass';
+  filter.frequency.value = 3000;
+  filter.Q.value = 0.8;
+
+  src.connect(filter);
+  filter.connect(gain);
+  gain.connect(ctx.destination);
+
+  src.buffer = buf;
+  gain.gain.setValueAtTime(volume, ctx.currentTime);
+  src.start(ctx.currentTime);
+}
